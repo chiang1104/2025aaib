0df3398a897463cdf99f57fd2fd9f042333be88d
蔣名曜 <91170675+chiang1104@users.noreply.github.com>
2025-03-05 11:46:09 +0800

    week03

---
+++ week03-3.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{///從0開始,不要超過 <15 就跑15次
+    for(int i=0 ; i<5 ; i++){
+        printf("%d ", i );
+    }///電腦的for迴圈 電腦從0開始
+    printf("\n");///跳行
+    for(int i=1;i<=5;i++){
+        printf("%d ", i );
+    }
+}
---
+++ week03_2_size_background_ellipse/week03_2_size_background_ellipse.pde
@@ -0,0 +1,4 @@
+//week03_2_size_background_ellipse
+size(400,400);//視窗大小
+background(255,255,242);//背景顏色
+ellipse(200,100,50,30);//橢圓位置大小
